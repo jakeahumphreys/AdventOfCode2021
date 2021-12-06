6ae94cc88d4e37610ac3207801177978de15f6ad
Jake H <jakehumphreys@outlook.com>
2021-12-06 21:26:21 +0000

    Project setup and day 1

---
+++ AdventOfCode2021/Days/Day 1/DayOne.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2021.Days
+{
+    public static class DayOne
+    {
+        public static void SonarSweep()
+        {
+            int largerMeasurements = 0;
+            var depthRecordings = File.ReadLines($"{AppDomain.CurrentDomain.BaseDirectory}..\\..\\..\\Days\\Day 1\\depthRecordings.txt").ToList();
+
+            var previousDepth = 0;
+            foreach (var depth in depthRecordings)
+            {
+                var depthAsInt = int.Parse(depth);
+                
+                if (previousDepth != 0 && depthAsInt > previousDepth)
+                {
+                    largerMeasurements += 1;
+                }
+                
+                previousDepth = depthAsInt;
+            }
+            
+            Console.WriteLine(largerMeasurements);
+        }
+    }
+}
---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AdventOfCode2021.Days;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select a day to run.");
+
+            var input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    DayOne.SonarSweep();
+                    break;
+            }
+        }
+    }
+}
